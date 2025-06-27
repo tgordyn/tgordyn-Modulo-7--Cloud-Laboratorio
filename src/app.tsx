f8a3d244d68752bd1a6415dd4461eb6d2d05fb22
tomasgordyn <tomas.gordyn@gmail.com>
2025-06-27 11:39:28 -0300

    cambios en app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/login";
 import { ListPage } from "./pages/list";
 import { DetailPage } from "./pages/detail";
 
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/list" element={<ListPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route path="*" element={<div>Not Found</div>} /> {/* Ruta 404 */}
+    </Routes>
   );
 };
